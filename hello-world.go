772f45adf192e29a98d21993611e40d3c08a6ca9
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-03-09 22:26:16 +0700

    use inline and multiline comment

---
+++ hello-world.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// komentar inline
+	/*
+		Komentar multiline
+		cocok digunakan untuk pesan komentar yang panjang
+	*/
+	fmt.Println("Hello World!");
+}
