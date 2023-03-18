ca467bc58a5b04a7f867ad891d1976f09cdf2be8
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-03-18 12:34:40 +0700

    operator golang

---
+++ a12-operator/1-aritmatika.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// Operator aritmatika adalah operator yang digunakan untuk operasi yang sifatnya perhitungan
+
+func main() {
+	var value = (((2 + 6) % 3) * 4 - 2) / 3
+	fmt.Print(value)
+}
